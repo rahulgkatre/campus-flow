8816d2b9b6f94ba6aa1ac2bdb4e130b6c321c6ba
Rahul Katre <rahulgkatre@gmail.com>
2022-04-13 14:38:30 -0400

    Added field normalization Co-authored-by: Mudit Gupta <muditg317@users.noreply.github.com>

---
+++ preprocessing/map_door_element.py
@@ -0,0 +1,10 @@
+import json
+
+class MapDoorElement:
+
+    def __init__(self, entry_points:list, building_name:str):
+        self.entry_points = entry_points
+        self.building_name = building_name
+
+    def json():
+                
